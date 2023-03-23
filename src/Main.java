8dc53bc0a1fdc72577ff1ab00d6e1e5d754bff3c
welnes222 <rokas222@hotmail.com>
2023-03-23 23:57:28 +0200

    pl

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         bookShelf.add(new Book("Pavasario selsmas", 444, 2013));
         bookShelf.add(new Book("Saulegrazu plentas", 149, 1987));
 
-       // System.out.print(bookShelf);
+        // System.out.print(bookShelf);
 
         for (int i = 0; i < bookShelf.size(); i++) {
             System.out.println(bookShelf.get(i));
@@ -38,15 +38,15 @@ public class Main {
 
         System.out.println("-----------------------------2-------------------------");
 
-        ArrayList<Book> garden = new ArrayList<>();
+        ArrayList<Plant> garden = new ArrayList<>();
 
         Plant a2 = new Plant();
         a2.setName("Blakine kalendra");
         a2.setLatinName("Coriandrum sativum");
         a2.setOneYear("Vientmetis");
-        a2.setGrowthRegion("Visur");
-        a2.h(0.6);
-        a2.edible("taip");
+        a2.setGrowthRegion("Europa");
+        a2.seth(1);
+        a2.setEdible("taip");
 
         garden.add(a2);
 
@@ -54,14 +54,17 @@ public class Main {
         b2.setName("Sosnovskio barštis");
         b2.setLatinName("Heracleum sosnowskyi");
         b2.setOneYear("Vientmetis");
-        b2.setGrowthRegion("Visur");
-        b2.h(5);
-        b2.edible("Ne");
+        b2.setGrowthRegion( "Europa" );
+        b2.seth(5);
+        b2.setEdible("Ne");
 
-        garden.add(a2);
+        garden.add(b2);
 
-        garden.add(new Plant("bambukas","Bambugu", "one year",  ))
+        garden.add(new Plant("bambukas", "Bambugu", "not one year", "Azija", 38, "ne"));
+        garden.add(new Plant("Šermukšnis", "Sorbus", " not one year", "Europa", 20, "ne"));
 
-
+        for (int i = 0; i < garden.size(); i++) {
+            System.out.println(garden.get(i));
+        }
     }
 }
--- src/Plant.java
+++ src/Plant.java
@@ -1,7 +1,7 @@
 public class Plant {
     private String name;
     private String latinName;
-    private boolean oneYear;
+    private String oneYear;
     private String growthRegion;
     private int h;
     private String edible;
@@ -10,53 +10,68 @@ public class Plant {
 
     }
 
-    public Plant(String name, String latinName, String oneYear, boolean growthRegion, int h, String edible) {
-    this.name =name;
-    this.latinName =latinName;
-    this.oneYear =oneYear;
-    this.growthRegion =growthRegion;
-    this.h =h;
-    this.edible =edible;
-}
-public String getName(){
-        return this.name;
-}
-public void setName(String name){
+    public Plant(String name, String latinName, String oneYear, String growthRegion, int h, String edible) {
         this.name = name;
-}
-public String getLatinName(){
-        return this.latinName;
-}
-public void setLatinName(String latinName){
         this.latinName = latinName;
-}
-public String getOneYear(String oneYear){
-        return this.oneYear;
-}
-
-public void setOneYear(String oneYear){
         this.oneYear = oneYear;
-}
-public boolean getGrowthregion(boolean growthRegion){
-        return this.growthRegion;
-}
-public void setGrowthRegion(boolean growthRegion){
         this.growthRegion = growthRegion;
-}
-public int geth(int h){
-        return this.h;
-}
-public void seth(int h){
         this.h = h;
-}
-public String getEdible(String edible){
+        this.edible = edible;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatinName() {
+        return this.latinName;
+    }
+
+    public void setLatinName(String latinName) {
+        this.latinName = latinName;
+    }
+
+    public String getOneYear(String oneYear) {
+        return this.oneYear;
+    }
+
+    public void setOneYear(String oneYear) {
+        this.oneYear = oneYear;
+    }
+
+    public String getGrowthregion(String growthRegion) {
+        return this.growthRegion;
+    }
+
+    public void setGrowthRegion(String growthRegion) {
+        this.growthRegion = growthRegion;
+    }
+
+    public int getH(int h) {
+        return this.h;
+    }
+
+    public void seth(int h) {
+        this.h = h;
+    }
+
+    public String getEdible(String edible) {
         return this.edible;
-}
-public void setEdible(String edible){
-        this.edible = edible
-}
+    }
+
+    public void setEdible(String edible) {
+        this.edible = edible;
+    }
+
+    @Override
+    public String toString() {
+        return ("Augalo pavadinimas: " + name + ", lotyniskas pavadinimas: "+ latinName + ", vienmetis ar nevienmetis: " +
+                oneYear + ", aukstis metrais: " + h + ", ar valgomas? " + edible);
 
 
-
-
+    }
 }
